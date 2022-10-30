9a622af2641a7ce36b98feca3460eab01c3093c3
urban freestyler <78681395+urbanfreestyler@users.noreply.github.com>
2022-10-30 21:35:12 +0500

    Update bot.py

--- mainapp/bot.py
+++ mainapp/bot.py
@@ -2,7 +2,7 @@ def send_application(name, phone, email):
     import telebot
     bot = telebot.TeleBot("TOKEN")
 
-    chat_id = "-1001662935535"
+    chat_id = "chat_id"
 
     bot.send_message(chat_id=chat_id, text="Новая заявка:\n"
                                                 "Имя: " + name + "\n"
